2889e242bbe243069853c6f3ef98214397bc2129
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-08-21 23:41:27 +0700

    add: star component and dinamic star

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import StarRating from './components/StarRating.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating max={5} />
   </StrictMode>,
 )
