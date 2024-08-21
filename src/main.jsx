22a97a8044ec38c8ae401d19546d98a2d791ab8d
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-08-22 00:01:40 +0700

    add: more dinamic on star

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import StarRating from './components/StarRating.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating max={5} />
+    <StarRating max={10} color="#fcc419" size={30} />
+    <StarRating color="red" size={160} />
+    <StarRating color="red" />
   </StrictMode>,
 )
